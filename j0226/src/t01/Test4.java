398dfc76525aee5e02ed10f98cb25a0cb9ef9eeb
NINANO <yunsk1121@naver.com>
2024-02-27 11:01:22 +0900

    first project

---
+++ j0226/src/t01/Test4.java
@@ -0,0 +1,16 @@
+package t01;
+
+public class Test4 {
+	//main 메소드
+	public static void main(String[] args) {
+		System.out.println("1.반\t가"); // \t는 탭
+		System.out.print("2.반가\n\n");
+		/*
+		이곳을
+		여러줄
+		주석 
+		*/
+		System.out.println("3.반가");
+		System.out.println("4.반가");
+	}
+}
